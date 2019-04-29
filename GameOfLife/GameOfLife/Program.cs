059cc8aef4a712ff84d3da4934ac584f5dbee38d
Student <Student@DESKTOP-6QVR8SB>
2019-04-29 19:30:02 +0200

    Rules

--- GameOfLife/GameOfLife/Program.cs
+++ GameOfLife/GameOfLife/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+/* 
+ * 1. 2D grid
+ * 2. Every cell is dead or alive 
+ * 3. At each new generation 
+ * 4. If a cell is alive 
+ * 5. Neighbours 0,1=Dead
+ * 6. Neighbours 2,3=Alive
+ * 7. Neighbours 4 or more = dead
+ * 8. If a cell is dead and Neighbours 3 = alive 
+ * 9. Otherwise cell = dead
+ * 
+ */
 namespace GameOfLife
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
