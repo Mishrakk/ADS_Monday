ee0879256e7b64883685120366f071fb2f781b10
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-13 19:01:54 +0200

    Beggining of UI

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -6,6 +6,10 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello, this is sorting program");
+            Console.WriteLine(" This is our array: ");
+            Console.WriteLine("This is sorted array");
         }
     }
 }
