062441fcc47d21f0293844466be2b81308887b21
Szymon <Student@DESKTOP-6QVR8SB>
2019-05-13 19:12:41 +0200

    Some more UI skeleton

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -6,10 +6,22 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-
+            int[] array = new int[]
+            {
+                5,2,7,8,9
+            };
             Console.WriteLine("Hello, this is sorting program");
-            Console.WriteLine(" This is our array: ");
+            Console.WriteLine("This is our array: ");
+            //Here print array before sort
+            PrintArry(array);
             Console.WriteLine("This is sorted array");
+            //Here print array after sort
+            PrintArry(array);
+
+        }
+        public static void PrintArry(int[] array)
+        {
+
         }
     }
 }
