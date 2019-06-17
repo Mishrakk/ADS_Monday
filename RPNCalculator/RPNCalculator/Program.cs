60de66b745ac123a281907d78c054f0303f78a28
Student <Student@DESKTOP-6QVR8SB>
2019-06-17 19:01:53 +0200

    Function signature

--- RPNCalculator/RPNCalculator/Program.cs
+++ RPNCalculator/RPNCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RPNCalculator
 {
@@ -6,6 +7,13 @@ namespace RPNCalculator
     {
         static void Main(string[] args)
         {
+            
+        }
+        public static double PostFixEvaluator(string Expression)
+        {
+
+            return 0;
+
         }
     }
 }
